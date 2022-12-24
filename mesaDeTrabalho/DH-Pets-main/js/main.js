a88a150dd09b1d63ee6eb10c7e0449477881965d
darlan <darlanoliv1@gmail.com>
2022-12-23 22:56:53 -0400

    pastas

---
+++ mesaDeTrabalho/DH-Pets-main/js/main.js
@@ -0,0 +1,28 @@
+// Mostrar a seção de Fale Conosco
+
+
+// Eventos NavLink - Gatos
+
+
+// Eventos NavLink - Cachorros
+
+
+// Eventos NavLink - Banner (Início da Página)
+
+
+// Eventos NavLink - Fale Conosco
+
+
+// Evento de submit do formulário Fale Conosco
+
+
+// Aqui você mostra a seção secreta
+
+// Aqui você deve pegar o botao-secreto (lembre-se que o nome dele deve ser botaoSecreto para o código abaixo funcionar)
+ 
+// Aqui você deverá pegar o input-secreto
+
+ 
+// Aqui você deverá pegar a mensagem-erro
+ 
+// Adicione aqui seu segredo personalizado (Micro desafio 2)
