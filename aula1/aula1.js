3b8f7d3f7955b6bb81699660c8a0008a1f916b52
darlan <darlanoliv1@gmail.com>
2023-01-02 19:38:58 -0400

    Exercício 1 Concluido

--- aula1/aula1.js
+++ aula1/aula1.js
@@ -1,1 +1,14 @@
-console.log("fazer exercicio1");
+let arrai = [1,2,3,4,5,6];
+ var cont = 0;
+
+    // if (arrai[0] < arrai[1]) {
+    cont = arrai[0] + arrai[1];
+        console.log(cont);
+        for (let i = 2; i < arrai.length; i++) {
+           cont = cont + arrai[i];
+          console.log(cont); 
+        }
+    
+    
+          
+
